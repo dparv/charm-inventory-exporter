4061e573abb5c8e413c44ecfea26224037eb35b5
Diko Parvanov <diko.parvanov@canonical.com>
2023-01-19 16:33:51 +0200

    Added new config options and relation handler

--- src/reactive/charm.py
+++ src/reactive/charm.py
@@ -4,39 +4,49 @@
 
 import logging
 
-from charms.reactive import set_flag, when, when_not, hook, endpoint_from_flag
+from charms.reactive import set_flag, when, when_not, hook, clear_flag
 from charms.layer import snap
 from charmhelpers.core import hookenv
-from charmhelpers.core.host import service_running
+from charmhelpers.core.templating import render
+from charmhelpers.core.host import service_running, service_restart
 
 logger = logging.getLogger(__name__)
+
+
+CONFIG_TEMPLATE = 'config.j2'
+CONFIG_FILE = '/var/snap/inventory-exporter/common/config.yaml'
+INVENTORY_EXPORTER_SERVICE = "snap.inventory-exporter.inventory-exporter"
 
 @when_not("inventory-exporter.installed")
 def install():
     """Install hook."""
     hookenv.status_set("maintenance", "Installing charm...")
-    if not validate_configs():
-        return
-    hookenv.open_port(8675, 'TCP')
     hookenv.status_set("active", "Unit is ready.")
     set_flag("inventory-exporter.installed") 
 
 @when("config.changed")
 def config_changed():
     """Config changed hook."""
-    #TODO: hookenv.model_name(); possibly via relation data toward collector?
-    if not validate_configs():
-        return
+    hookenv.open_port(hookenv.config("port"), 'TCP')
+    clear_flag('inventory-exporter.config.rendered')
     hookenv.status_set("active", "Unit is ready.")
+
+@when("config.changed.port")
+def port_changed():
+    for full_port in hookenv.opened_ports():
+        port = full_port.split("/")[0]
+        hookenv.close_port(port, 'TCP')
+    hookenv.open_port(hookenv.config("port"), 'TCP')
+    clear_flag('inventory-exporter.config.rendered')
+    configure_inventory_exporter_relation()
 
 @hook("update-status")
 def update_status():
     """Update status hook."""
-    inventory_exporter_service = "snap.inventory-exporter.inventory-exporter"
-    running = service_running(inventory_exporter_service)
+    running = service_running(INVENTORY_EXPORTER_SERVICE)
     if not running:
-        error = inventory_exporter_service + " service not running"
-        hookenv.status_set("active", error)
+        error = INVENTORY_EXPORTER_SERVICE + " service not running"
+        hookenv.status_set("blocked", error)
         return
     hookenv.status_set("active", "Unit is ready.")
 
@@ -45,15 +55,35 @@ def stop():
     """Stop hook."""
     snap.remove("inventory-exporter")
 
-def validate_configs():
-    """Validate the charm config options."""
-    customer = hookenv.config("customer")
-    site = hookenv.config("site")
-    if not customer:
-        hookenv.status_set("blocked", "Need to set 'customer' config")
-        return False
-    if not site:
-        hookenv.status_set("blocked", "Need to set 'site' config")
-        return False
+@hook('inventory-exporter-relation-joined', 'inventory-exporter-relation-changed')
+def configure_inventory_exporter_relation(relation_id=None, unit=None):
+    """Provide relation data."""
+    relations = hookenv.relation_ids("inventory-exporter")
+    if not relations:
+        return
+    local_unit_address = hookenv.unit_private_ip()
+    relation_settings = {
+        "port": hookenv.config('port'),
+        "model": hookenv.model_name(),
+    }
+    for rid in relations:
+        hookenv.relation_set(
+            rid,
+            relation_settings=relation_settings,
+        )
+    hookenv.status_set('active', 'Unit is ready.')
 
-    return True
+@when_not('inventory-exporter.config.rendered')
+def render_config():
+    """Render the exporter snap config."""
+    context = {
+        'bind_address': hookenv.config("bind_address"),
+        'port': hookenv.config("port"),
+        }
+    render(
+        source=CONFIG_TEMPLATE,
+        target=CONFIG_FILE,
+        context=context,
+    )
+    service_restart(INVENTORY_EXPORTER_SERVICE)
+    set_flag('inventory-exporter.config.rendered')
